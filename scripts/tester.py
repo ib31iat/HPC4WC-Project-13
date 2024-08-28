38aa1439af25d978ad5d63bdf0fbb08cfe604ed8
Tobias R <rtobias286@gmail.com>
2024-08-28 22:39:29 +0200

    Update `tester.py`: Improve file name

--- scripts/tester.py
+++ scripts/tester.py
@@ -16,7 +16,7 @@ range_ny = [128, 256]
 range_nz = [64, 256]
 range_num_iter = [128, 512]
 range_precision = ["32", "64"]
-functions = [jax_base_calc, jax_calc, numpy_calc, torch_calc, numba_calc]
+functions = {"jax_base": jax_base_calc, "jax": jax_calc, "numpy": numpy_calc, "torch": torch_calc, "numba": numba_calc}
 
 results = {}
 
@@ -32,9 +32,9 @@ for filename in os.listdir(folder):
 def tester():
     for nx, ny, nz, num_iter in zip(range_nx, range_ny, range_nz, range_num_iter):
         for p in range_precision:
-            for f in functions:
-                for _ in range(num_reps):
-                    results[f"{num_reps}_nx{nx}_ny{ny}_nz{nz}_num_iter{num_iter}_p{p}_f{f.__name__}"] = f(
+            for n, f in functions:
+                for r in range(num_reps):
+                    results[f"{r}_nx{nx}_ny{ny}_nz{nz}_num_iter{num_iter}_p{p}_{n}"] = f(
                         nx, ny, nz, num_iter, 2, p, return_result=False, return_time=True
                     )
             with open(f"../results_tmp/{datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl", "wb") as f_out:
