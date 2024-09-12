21c59069c4d320acafc3800bc9e7c9e425847058
FStefanetti <113979455+FStefanetti@users.noreply.github.com>
2024-09-12 20:13:33 +0200

    Update stencil2d_numba_stencil_vectorize.py

--- scripts/stencil2d_numba_stencil_vectorize.py
+++ scripts/stencil2d_numba_stencil_vectorize.py
@@ -1,46 +1,97 @@
+# ******************************************************
+#     Program: stencil2d
+#      Author: Tobias Rahn
+#       Email: tobias.rahn@inf.ethz.ch
+#        Date: 28.06.2024
+# Description: 4th-order diffusion
+#        Note: Based on https://github.com/ofuhrer/HPC4WC/blob/main/day1/stencil2d.py
+# ******************************************************
+
 import os
 import sys
 import click
 import numpy as np
 import time
 from datetime import datetime
-from numba import stencil, vectorize, njit, float32, float64
 
 
-# Define the stencil for the Laplacian operation
-@stencil
-def laplacian_kernel(in_field):
-    return -4.0 * in_field[0, 0, 0] + in_field[0, 0, -1] + in_field[0, 0, 1] + in_field[0, -1, 0] + in_field[0, 1, 0]
+def laplacian(in_field, lap_field, num_halo, extend=0):
+    """Compute the Laplacian using 2nd-order centered differences.
+
+    Parameters
+    ----------
+    in_field : array-like
+        Input field (nz x ny x nx with halo in x- and y-direction).
+    lap_field : array-like
+        Result (must be same size as ``in_field``).
+    num_halo : int
+        Number of halo points.
+    extend : `int`, optional
+        Extend computation into halo-zone by this number of points.
+    """
+    ib = num_halo - extend
+    ie = -num_halo + extend
+    jb = num_halo - extend
+    je = -num_halo + extend
+
+    lap_field[:, jb:je, ib:ie] = (
+        -4.0 * in_field[:, jb:je, ib:ie]
+        + in_field[:, jb:je, ib - 1 : ie - 1]
+        + in_field[:, jb:je, ib + 1 : ie + 1 if ie != -1 else None]
+        + in_field[:, jb - 1 : je - 1, ib:ie]
+        + in_field[:, jb + 1 : je + 1 if je != -1 else None, ib:ie]
+    )
 
 
-# Use vectorize for the update operation
-@vectorize([float32(float32, float32, float32), float64(float64, float64, float64)], target="parallel")
-def update_field(in_val, lap_val, alpha):
-    return in_val - alpha * lap_val
-
-
-@njit(parallel=True, fastmath=True)
 def update_halo(field, num_halo):
+    """Update the halo-zone using an up/down and left/right strategy.
+
+    Parameters
+    ----------
+    field : array-like
+        Input/output field (nz x ny x nx with halo in x- and y-direction).
+    num_halo : int
+        Number of halo points.
+
+    Note
+    ----
+        Corners are updated in the left/right phase of the halo-update.
+    """
+    # bottom edge (without corners)
     field[:, :num_halo, num_halo:-num_halo] = field[:, -2 * num_halo : -num_halo, num_halo:-num_halo]
+    # top edge (without corners)
     field[:, -num_halo:, num_halo:-num_halo] = field[:, num_halo : 2 * num_halo, num_halo:-num_halo]
+    # left edge (including corners)
     field[:, :, :num_halo] = field[:, :, -2 * num_halo : -num_halo]
+    # right edge (including corners)
     field[:, :, -num_halo:] = field[:, :, num_halo : 2 * num_halo]
-    return field
 
 
 def apply_diffusion(in_field, out_field, alpha, num_halo, num_iter=1):
+    """Integrate 4th-order diffusion equation by a certain number of iterations.
+
+    Parameters
+    ----------
+    in_field : array-like
+        Input field (nz x ny x nx with halo in x- and y-direction).
+    lap_field : array-like
+        Result (must be same size as ``in_field``).
+    alpha : float
+        Diffusion coefficient (dimensionless).
+    num_iter : `int`, optional
+        Number of iterations to execute.
+    """
+    tmp_field = np.empty_like(in_field)
+
     for n in range(num_iter):
-        # Apply halo updates before stencil operation
-        in_field = update_halo(in_field, num_halo)
+        update_halo(in_field, num_halo)
 
-        # Apply the stencil for the Laplacian
-        lap_field = laplacian_kernel(in_field)
+        laplacian(in_field, tmp_field, num_halo=num_halo, extend=1)
+        laplacian(tmp_field, out_field, num_halo=num_halo, extend=0)
 
-        # Update the field with the vectorized function
-        out_field[:, num_halo:-num_halo, num_halo:-num_halo] = update_field(
-            in_field[:, num_halo:-num_halo, num_halo:-num_halo],
-            lap_field[:, num_halo:-num_halo, num_halo:-num_halo],
-            alpha,
+        out_field[:, num_halo:-num_halo, num_halo:-num_halo] = (
+            in_field[:, num_halo:-num_halo, num_halo:-num_halo]
+            - alpha * out_field[:, num_halo:-num_halo, num_halo:-num_halo]
         )
 
         if n < num_iter - 1:
@@ -52,17 +103,17 @@ def apply_diffusion(in_field, out_field, alpha, num_halo, num_iter=1):
 
 
 def calculations(nx, ny, nz, num_iter, num_halo, precision, result_dir="", return_result=False, return_time=False):
+    """Driver for apply_diffusion that sets up fields and does timings"""
     assert 0 < nx <= 1024 * 1024, "You have to specify a reasonable value for nx"
     assert 0 < ny <= 1024 * 1024, "You have to specify a reasonable value for ny"
     assert 0 < nz <= 1024, "You have to specify a reasonable value for nz"
     assert 0 < num_iter <= 1024 * 1024, "You have to specify a reasonable value for num_iter"
     assert 2 <= num_halo <= 256, "You have to specify a reasonable number of halo points"
-
     alpha = 1.0 / 32.0
 
     dtype = np.float64 if precision == "64" else np.float32
+
     in_field = np.zeros((nz, ny + 2 * num_halo, nx + 2 * num_halo), dtype=dtype)
-    in_field = np.ascontiguousarray(in_field)
     in_field[
         nz // 4 : 3 * nz // 4,
         num_halo + ny // 4 : num_halo + 3 * ny // 4,
@@ -70,8 +121,10 @@ def calculations(nx, ny, nz, num_iter, num_halo, precision, result_dir="", retur
     ] = 1.0
     out_field = np.copy(in_field)
 
+    # warmup caches
     apply_diffusion(in_field, out_field, alpha, num_halo)
 
+    # time the actual work
     tic = time.time()
     out_field = apply_diffusion(in_field, out_field, alpha, num_halo, num_iter=num_iter)
     toc = time.time()
@@ -101,13 +154,13 @@ def calculations(nx, ny, nz, num_iter, num_halo, precision, result_dir="", retur
 @click.option(
     "--num_halo",
     type=int,
-    default=4,
+    default=2,
     help="Number of halo-pointers in x- and y-direction",
 )
 @click.option(
     "--result_dir",
     type=str,
-    default="../data/numba",
+    default="../data/numpy",
     help="Specify the folder where the results should be saved (relative to the location of the script or absolute).",
 )
 def main(nx, ny, nz, num_iter, result_dir, num_halo, precision):
